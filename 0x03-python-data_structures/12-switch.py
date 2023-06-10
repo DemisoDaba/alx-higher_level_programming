7211d604ed5708d4641d33004a92e99b14b01ce7
DemisoDaba <demo.nkmt1@gmail.com>
2023-06-10 03:53:54 -0700

    Complete the source code in order to switch value of a and b

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+def switch_values(a, b):
+    a, b = b, a
+    return a, b
+
 a = 89
 b = 10
-a, b = b, a
+
+print("Before switching:")
+print("a={:d} - b={:d}".format(a, b))
+
+a, b = switch_values(a, b)
+
+print("After switching:")
 print("a={:d} - b={:d}".format(a, b))
