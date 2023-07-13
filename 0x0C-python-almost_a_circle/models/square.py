d810a488c7bf1ff374c3cd9f89a99280967c33b4
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-07-13 21:06:29 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,45 +1,57 @@
 #!/usr/bin/python3
-
 """
-Module: `square`
+square module
 """
-
-
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Class: `Square`"""
+    """
+    Square implementation
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """Init method"""
+        """
+        init - initilizer
+        Args:
+            size: size of the square
+            x: x position of the square
+            y: y position of the square
+            id: id of the square
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Str method"""
+        """
+        overriden __str__ method
+        """
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+                self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Getter for size"""
+        """size getter"""
         return self.width
 
     @size.setter
-    def size(self, value):
-        """Setter for size"""
-        self.width = value
-        self.height = value
+    def size(self, size):
+        """size setter"""
+        self.width = size
+        self.height = size
 
     def update(self, *args, **kwargs):
-        """Update method"""
+        """
+        assigns attributes
+        """
+        lst = (self.id, self.size, self.x, self.y)
         if args:
-            attrs = ["id", "size", "x", "y"]
-            for i in range(len(args)):
-                setattr(self, attrs[i], args[i])
-        else:
-            for key, value in kwargs.items():
+            self.id, self.size, self.x, self.y = \
+                    args + lst[len(args):len(lst)]
+        elif kwargs:
+            for (key, value) in kwargs.items():
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """To dictionary method"""
-        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        """
+        that returns the dictionary representation of a Square.
+        """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
