65edc601c45f30cfa9bf6d1e39daacc3137442c2
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-11-20 14:52:56 +0300

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
 """
-Lists all City objects from the database hbtn_0e_101_usa.
-Usage: ./102-relationship_cities_states_list.py <mysql username> /
-                                                <mysql password> /
-                                                <database name>
+Lists all City objects from the database hbtn_0e_101_usa
 """
-
 import sys
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import City
 
-if __name__ == "__main__":
-    # Check if all required command line arguments are provided.
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Create a database engine using command line arguments.
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
-    # Create a session maker and establish a session with the database.
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and list all City objects ordered by their IDs, including State information.
-    cities = session.query(City).order_by(City.id).all()
+    st = session.query(State).join(City).order_by(City.id).all()
 
-    for city in cities:
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-
-
+    for state in st:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
